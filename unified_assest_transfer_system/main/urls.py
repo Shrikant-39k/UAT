c0cd5cbd9f8c13397427a191a3efa68baf9897f2
shrikant-39k <shrikant@39kpartners.com>
2025-08-01 12:14:56 +0530

    initial commit for new branch.

--- unified_assest_transfer_system/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CfeView
-
-urlpatterns = [
-    # path('', LoginView.as_view(), name='login'),
-    path('cfe/', CfeView.as_view(), name='cfe'),
-    
-]
